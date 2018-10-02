4c425d71153da56c965cb032e5c51af3325efda0
Grayson Lorenz <LorenzWoodShop@gmail.com>
2018-10-02 15:57:42 -0500

    board calculator
    added it

---
+++ js/boardCalculator.js
@@ -0,0 +1,52 @@
+//Contact Code
+angular.module('woodApp')
+.factory('BoardCalculatorFactory', function () {
+    var factory = {};
+    var standards = [
+        { label: 'in.' },
+        { label: 'cm.' }
+    ];
+
+    factory.getStandards = function() {
+        return standards;
+    };
+
+    return factory;
+})
+
+.controller('BoardCalculatorController', function ($scope, BoardCalculatorFactory) {
+    $scope.standards = BoardCalculatorFactory.getStandards();
+    $scope.cart = [];
+    $scope.cartTotal = 0;
+
+    $scope.addToCart = function () {
+        var thickness = $scope.numThickness;
+        var width = $scope.numWidth;
+        var length = $scope.numLength;
+        var species = $scope.txtSpecies;
+        var price = $scope.numPrice
+        var inToCm = 2.54;
+        if ($scope.selectedThicknessStandard.label === 'cm.') {
+            thickness /= inToCm;
+        }
+        if ($scope.selectedWidthStandard.label === 'cm.') {
+            width /= inToCm;
+        }
+        if ($scope.selectedLengthStandard.label === 'cm.') {
+            length /= inToCm;
+        }
+        var total = ((thickness * width * length) / 144) * price;
+
+        $scope.cart.push(
+            { species: species, 
+            thickness: $scope.numThickness + ' ' + $scope.selectedThicknessStandard.label,
+            width: $scope.numWidth + ' ' + $scope.selectedWidthStandard.label,
+            length: $scope.numLength + ' ' + $scope.selectedLengthStandard.label,
+            price: price,
+            total: total
+            }
+        );
+
+        $scope.cartTotal += total;
+    }
+});
